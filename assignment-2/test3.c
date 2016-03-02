f993b4032fe2f47ffd5a6bc0b8f276121e2391f2
Matthias Larsen <mlarsen@gammamedia.org>
2016-03-02 20:57:02 +0100

    Added another method to test3.c file

--- assignment-2/test3.c
+++ assignment-2/test3.c
@@ -7,4 +7,12 @@ int main( )
 	  printf("Hello World 2!\n");
 	  printf("Hello World!\n");
   }
+  return 0;
+}
+
+int anotherMethod( )
+{
+  printf("Hello World!\n");
+  printf("Hello World!\n");
+  return 0;
 }
