4b4bad2ffa586d1aadfd98ccf487db52d3fb3ba5
Matthias Larsen <mlarsen@gammamedia.org>
2016-05-31 10:44:13 +0200

    Clean up

--- assignment-4/test3.c
+++ assignment-4/test3.c
@@ -11,12 +11,9 @@ int main( )
   int a = 2;
   int b = 3;
   add(a, a, a);
-  add(b, a, a);
-  //int cond = 1;
   if (a + b == 1) {
-    printf("Hello World!\n");
-  } else {
-    printf("Hello World!\n");
+    printf("Hello World!!\n");
+    add(a, a, b);
   }
   return 0;
 }
